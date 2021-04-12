ff1c431764c0a27e9ef272594c2e1a02158b5ec1
kamestart <ajmeramanit230734@gmail.com>
2021-04-12 14:41:57 +0530

    some simple mistakes :(

--- server.js
+++ server.js
@@ -18,30 +18,26 @@ const user = require('./src/models/user')
 // routers
 const indexRouter = require('./src/routes/index')
 const videoRouter = require('./src/routes/videos')
-const userRouter = require('./src/routes/user')
 
 // set the settings and configure routers, views, etc.
 app.disable('x-powered-by')
 app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
+app.set('views', __dirname + '/src/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/src/public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json({ limit: "8gb", extended: false }))
 app.use(methodOveride('_method'))
 app.use(cookieParser())
 app.use('/videos', videoRouter)
 app.use('/', indexRouter)
-app.use('/users', userRouter)
 
 
 // connect to mongoose
 
 mongoose.connect(process.env.DATABASE_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
 
-
-app.listen(process.env.PORT || 3000)
 
 app.get('/ids/create_id', async (req, res) => {
     try {
@@ -58,7 +54,7 @@ app.get('/ids/create_id', async (req, res) => {
 
 })
 
-// ------------------------------------------Code for the login and signup----------------------------------------------
+// ------------------------------------------ Code for the login and signup ----------------------------------------------
 
 // @funtion findOneModel
 // @desc A function which finds a model and takes in a model, it's parameter and its value
@@ -67,7 +63,7 @@ function findOneModel(model, parameterq, value) {
     return modelToReturn
 }
 
-app.get('/signUp', (req, res) => {
+app.get('/register', (req, res) => {
     console.log("Signup process initialized")
     res.render('signUp', { production: productiono })
 })
@@ -75,7 +71,7 @@ app.get('/signUp', (req, res) => {
     // @route users/Signup
     // @desc signup logic
 
-app.post('/signUp', async (req, res) => {
+app.post('/register', async (req, res) => {
     try {
         const password = req.body.password
         const hashedPassword = await bcrypt.hash(password, 15)
@@ -107,7 +103,7 @@ app.get('/login', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    
+
 })
 
 
@@ -115,3 +111,7 @@ app.get('/oneUser/:username', async (req, res) => {
     const userToSend = findOneModel(user, "username", req.params.username)
     res.render('oneUser', { user: userToSend })
 })
+
+
+
+app.listen(process.env.PORT || 3000)
