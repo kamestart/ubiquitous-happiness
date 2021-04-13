7274e510186331235786619cf9c25366dd01829a
kamestart <ajmeramanit230734@gmail.com>
2021-04-13 09:02:56 +0530

    silly mistake

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV != "production" ? require('dotenv').config() : console.log("In Prod Mode!")
+process.env.NODE_ENV != "production" ? require('dotenv').config() : console.log(":)")
 
 // modules
 const express = require('express')
@@ -77,5 +77,4 @@ app.get('/chat', (re, res) => {
     res.render('chat')
 })
 
-
-server.listen(process.env.PORT || 3000 , () => { console.log(`Listening on port ${PORT}`) })
+server.listen(process.env.PORT || 3000, () => { console.log(`Listening on port ${process.env.PORT || 3000}`) })
